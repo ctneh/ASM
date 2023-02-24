c7eb84204e833021504a436216d0096268de41a2
ctneh <126220546+ctneh@users.noreply.github.com>
2023-02-24 10:38:45 +0700

    Add files via upload

---
+++ ASM_Final/db/Students.js
@@ -0,0 +1,32 @@
+﻿[
+  {
+    "username": "han",
+    "password": "iloveyou",
+    "fullname": "Nguyễn Thị Ngọc Hân",
+    "email": "hanntnps21864@fpt.edu.vn",
+    "gender": "false",
+    "birthday": "2003-03-30",
+    "schoolfee": "1500000",
+    "marks": "0"
+  },
+  {
+    "username": "pheonv",
+    "password": "iloveyou",
+    "fullname": "Nguyễn Văn Chí Phèo",
+    "email": "pheonv@fpt.edu.vn",
+    "gender": "true",
+    "birthday": "1985-10-11",
+    "schoolfee": "2500000",
+    "marks": "0"
+  },
+  {
+    "username": "nopt",
+    "password": "iloveyou",
+    "fullname": "Phạm Thị Nở",
+    "email": "nopt@fpt.edu.vn",
+    "gender": "false",
+    "birthday": "1993-05-15",
+    "schoolfee": "2000000",
+    "marks": "0"
+  }
+]
